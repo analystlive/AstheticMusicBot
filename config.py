f2b67dd9564f6546f92566151c8a47502602941d
analystlive <alokbidi@outlook.com>
2025-12-26 10:50:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+OWNER_ID = "8247235878"   # tera user id
